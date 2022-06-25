520159bef85d87b81066e47263d2331c9627729d
Andreas <andreas95560@gmail.com>
2022-06-25 18:14:26 +0200

    separate filtersdata / filterstemplate

--- src/app/components/filtersComponents.js
+++ src/app/components/filtersComponents.js
@@ -2,9 +2,10 @@ import { FiltersController } from '../controller/filtersController';
 import { CreateElement } from './../utils/createElement';
 import { SearchComponents } from './searchComponents';
 
-export class FiltersComponents {
+export class FiltersComponents extends FiltersController {
 
      constructor(data) {
+          super(data)
           this.sectionFilters = document.querySelector('.section-filters');
           this.data = data
           this.categories = [
@@ -42,31 +43,15 @@ export class FiltersComponents {
                     ${this.displayItems(categoryName)}
                </div>
              `
-
           })
 
           const bts = document.querySelector('.adv-search-wrapper');
           bts.classList.add('row', 'col-12', 'col-lg-12');
 
-
-
           return this.tbn
      }
 
-     searchItemWithInput(categoryName) {
-          const searchInput = document.querySelector(`#dropdownMenuButton${categoryName}`);
-          searchInput.addEventListener('keyup', () => {
-               const value = searchInput.value.toLowerCase();
-               const items = document.querySelectorAll(`.dropdown-item`);
-               items.forEach(el => {
-                    if (el.textContent.toLowerCase().indexOf(value) != -1) {
-                         el.style.display = 'block';
-                    } else {
-                         el.style.display = 'none';
-                    }
-               })
-          })
-     }
+
 
      clickOnItem(categoryName, color) {
           const list = document.querySelector(`#list-${categoryName}`);
@@ -86,48 +71,5 @@ export class FiltersComponents {
 
      }
 
-     test(value, data) {
 
-          this.tabdeux = []
-
-          if (data === undefined || data === null || data.length <= 0) {
-               data = this.data
-          }
-
-          for (let el of data) {
-               if (el.ingredients.some(ing => ing.ingredient.toLowerCase() === value) ||
-                    el.appliance.toLowerCase() === value ||
-                    el.ustensils.some(ust => ust.toLowerCase() === value)) {
-
-                    this.tabdeux.push(el)
-
-               }
-          }
-          return new SearchComponents(this.tabdeux).setList()
-     }
-
-     displayItems(categoryName) {
-          if (categoryName === 'ingredients') {
-               this.ingredients = []
-               this.data.forEach(el => {
-                    el.ingredients.map(ing => this.ingredients.push(ing.ingredient.toLowerCase()));
-               })
-               const filteredArray = this.ingredients.filter((ele, pos) => this.ingredients.indexOf(ele) == pos);
-               return filteredArray.slice(0, 50).map(el => `<li value='${el}' class="dropdown-item ingItem" href="#">${el}</li>`).join('')
-          } else if (categoryName === 'appliance') {
-               this.appareils = []
-               this.data.forEach(el => {
-                    this.appareils.push(el.appliance.toLowerCase())
-               })
-               const filteredArray = this.appareils.filter((ele, pos) => this.appareils.indexOf(ele) == pos);
-               return filteredArray.map(el => `<li value='${el}' class="dropdown-item appItem" href="#">${el}</li>`).join('')
-          } else if (categoryName === 'ustensils') {
-               this.ustensiles = []
-               this.data.forEach(el => {
-                    el.ustensils.map(ing => this.ustensiles.push(ing.toLowerCase()));
-               })
-               const filteredArray = this.ustensiles.filter((ele, pos) => this.ustensiles.indexOf(ele) == pos);
-               return filteredArray.map(el => `<li value='${el}' class="dropdown-item ustItem" href="#">${el}</li>`).join('')
-          }
-     }
 }
--- src/app/controller/filtersController.js
+++ src/app/controller/filtersController.js
@@ -1,8 +1,71 @@
+import { SearchComponents } from './../components/searchComponents';
+
 export class FiltersController {
 
      constructor(data) {
           this.data = data
      }
 
+     test(value, data) {
+
+          this.tabdeux = []
+
+          if (data === undefined || data === null || data.length <= 0) {
+               data = this.data
+          }
+
+          for (let el of data) {
+               if (el.ingredients.some(ing => ing.ingredient.toLowerCase() === value) ||
+                    el.appliance.toLowerCase() === value ||
+                    el.ustensils.some(ust => ust.toLowerCase() === value)) {
+
+                    this.tabdeux.push(el)
+
+               }
+          }
+          return new SearchComponents(this.tabdeux).setList()
+     }
+
+     displayItems(categoryName) {
+          if (categoryName === 'ingredients') {
+               this.ingredients = []
+               this.data.forEach(el => {
+                    el.ingredients.map(ing => this.ingredients.push(ing.ingredient.toLowerCase()));
+               })
+               const filteredArray = this.ingredients.filter((ele, pos) => this.ingredients.indexOf(ele) == pos);
+               return filteredArray.slice(0, 50).map(el => `<li value='${el}' class="dropdown-item ingItem" href="#">${el}</li>`).join('')
+          } else if (categoryName === 'appliance') {
+               this.appareils = []
+               this.data.forEach(el => {
+                    this.appareils.push(el.appliance.toLowerCase())
+               })
+               const filteredArray = this.appareils.filter((ele, pos) => this.appareils.indexOf(ele) == pos);
+               return filteredArray.map(el => `<li value='${el}' class="dropdown-item appItem" href="#">${el}</li>`).join('')
+          } else if (categoryName === 'ustensils') {
+               this.ustensiles = []
+               this.data.forEach(el => {
+                    el.ustensils.map(ing => this.ustensiles.push(ing.toLowerCase()));
+               })
+               const filteredArray = this.ustensiles.filter((ele, pos) => this.ustensiles.indexOf(ele) == pos);
+               return filteredArray.map(el => `<li value='${el}' class="dropdown-item ustItem" href="#">${el}</li>`).join('')
+          }
+     }
+
+     searchItemWithInput(categoryName) {
+          const searchInput = document.querySelector(`#dropdownMenuButton${categoryName}`);
+          searchInput.addEventListener('keyup', () => {
+               const value = searchInput.value.toLowerCase();
+               const items = document.querySelectorAll(`.dropdown-item`);
+
+               for (let el of items) {
+                    if (el.textContent.toLowerCase().indexOf(value) != -1) {
+                         el.style.display = 'block';
+                    } else {
+                         el.style.display = 'none';
+                    }
+               }
+
+          })
+     }
 
 }
